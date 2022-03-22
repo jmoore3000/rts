e74ff22c2f3654e7742f424d2cb12b4d65b739cc
jmoore3000 <fender655@gmail.com>
2022-03-21 20:27:01 -0600

    compleatly overhauled the contact form using java.

---
+++ js/java.js
@@ -0,0 +1,53 @@
+//declarations
+    const firstName = document.querySelector("#firstName")
+    const lastName = document.querySelector("#lastName")
+    const telephone = document.querySelector("#telephone")
+    const email = document.querySelector("#email")
+    const message = document.querySelector("#message")
+    const success = document.querySelector("#success")
+    const errorNodes = document.querySelectorAll(".error")
+//validate data
+    function validateForm(){
+        
+        clearMessages();
+        let errorFlag = false
+        
+        if(firstName.value.length < 1){
+            errorNodes[0].innerText = "Name Cannot Be Blank.";
+            firstName.classList.add("error-border");
+            errorFlag = true
+        }
+
+        if(message.value.length < 1){
+            errorNodes[1].innerText = "Please Include a Message.";
+            message.classList.add("error-border");
+            errorFlag = true
+        }
+
+        if(!emailIsValid(email.value)){
+            errorNodes[2].innerText = "E-mail Not Valid.";
+            email.classList.add("error-border"); 
+            errorFlag = true
+        }
+
+        if(!errorFlag){
+            success.innerText = "Success!";
+        }
+}
+
+//clearing error and success messages
+    function clearMessages(){
+        for(let i=0; i < errorNodes.length; i++){
+            errorNodes[i].innerText = "";
+        }
+        success.innerText = "";
+        firstName.classList.remove("error-border");
+        message.classList.remove("error-border");
+        email.classList.remove("error-border");
+    }
+
+//checking email validity
+    function emailIsValid(email){
+        let pattern = /\S+@\S+\.\S+/;
+        return pattern.test(email);
+    }
